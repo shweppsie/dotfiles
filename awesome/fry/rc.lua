30d42d8f694bafc116b83c45ecc61c10f2b5a4ac
Nathan Overall <git@shweppsie.com>
2011-12-28 02:09:05 +1300

    [awesome] Deal with errors in the awesome config more gracefully

--- awesome/fry/rc.lua
+++ awesome/fry/rc.lua
@@ -10,6 +10,15 @@ require("naughty")
 -- Load Debian menu entries
 require("debian.menu")
 
+-- {{{ Error handling
+-- Check if awesome encountered an error during startup and fell back to
+-- another config (This code will only ever execute for the fallback config)
+if awesome.startup_errors then
+    naughty.notify({ preset = naughty.config.presets.critical,
+                     title = "Oops, there were errors during startup!",
+                     text = awesome.startup_errors })
+end
+
 function volume (mode, widget)
 	local command = ".config/awesome/vol.sh"
 	if mode == "up" then
@@ -23,6 +32,22 @@ function volume (mode, widget)
 	
 	widget.text = " -[ " .. volume .. " ]+ "
 end
+
+-- Handle runtime errors after startup
+do
+    local in_error = false
+    awesome.add_signal("debug::error", function (err)
+        -- Make sure we don't go into an endless error loop
+        if in_error then return end
+        in_error = true
+
+        naughty.notify({ preset = naughty.config.presets.critical,
+                         title = "Oops, an error happened!",
+                         text = err })
+        in_error = false
+    end)
+end
+-- }}}
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, and wallpapers

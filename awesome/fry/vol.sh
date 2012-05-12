b1b89681c7d010503cec22466069e862c935bb87
Nathan Overall <git@shweppsie.com>
2012-05-12 23:17:07 +1200

    [awesome] Improve resilience to changes in device ids

--- awesome/fry/vol.sh
+++ awesome/fry/vol.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# Description of device
+DESC="Internal Audio Analog Stereo"
+
 [[ $1 == down ]] && state="down"
 [[ $1 == up ]] && state="up"
 [[ $2 ]] && increment=$2 || increment=1
 
-# get the primary output sound device
-sink_id=`pactl list | grep -A 2 'Sink #0' | grep 'Name: ' | sed 's/.*Name: \(.*\)$/\1/g'`
+# get the sink info
+sink_id=`pactl list | pcregrep -A 10 -M "Sink #[0-9]+\n\tState: RUNNING\n\tName: .*\n\tDescription: ${DESC}" | head -n 1 | sed 's/^Sink #\([0-9]\+\)$/\1/g'`
 
 # increase or decrese the volume
 if [[ "$state" == "down" ]]; then
@@ -14,6 +17,6 @@ elif [[ "$state" == "up" ]]; then
 	pactl set-sink-volume $sink_id -- +${increment}% > /dev/null
 fi
 
-# Print the current volume
-echo `pactl list | grep -B 2 -A 10 "Name: ${sink_id}\$" | grep "[^ ]Volume:" | grep -o '[0-9]*%' | head -n 1`
+# print out the volume after any changes
+pactl list | pcregrep -A 10 -M "Sink #[0-9]+\n\tState: RUNNING\n\tName: .*\n\tDescription: ${DESC}" | grep -P '\tVolume: ' | grep -o '[0-9]\+%' | head -n 1
 

fe3c7d816a6e5795bacfd9e9f6217ada0b54396e
Nathan Overall <git@shweppsie.com>
2012-06-07 02:01:13 +1200

    [awesome] Make volume script work again

--- awesome/fry/vol.sh
+++ awesome/fry/vol.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
 # Description of device
-DESC="Internal Audio Analog Stereo"
+DESC="Built-in Audio Analog Stereo"
 
-[[ $1 == down ]] && state="down"
-[[ $1 == up ]] && state="up"
+REGEX="Sink #[0-9]+\n\tState: (RUNNING|SUSPENDED)\n\tName: .*\n\tDescription: ${DESC}"
+
 [[ $2 ]] && increment=$2 || increment=1
 
 # get the sink info
-sink_id=`pactl list | pcregrep -A 10 -M "Sink #[0-9]+\n\tState: RUNNING\n\tName: .*\n\tDescription: ${DESC}" | head -n 1 | sed 's/^Sink #\([0-9]\+\)$/\1/g'`
+sink_id=`pactl list | pcregrep -A 10 -M "${REGEX}" | head -n 1 | sed 's/^Sink #\([0-9]\+\)$/\1/g'`
 
 # increase or decrese the volume
-if [[ "$state" == "down" ]]; then
+if [[ "$1" == "down" ]]; then
 	pactl set-sink-volume $sink_id -- -${increment}% > /dev/null
-elif [[ "$state" == "up" ]]; then
+elif [[ "$1" == "up" ]]; then
 	pactl set-sink-volume $sink_id -- +${increment}% > /dev/null
 fi
 
-# print out the volume after any changes
-pactl list | pcregrep -A 10 -M "Sink #[0-9]+\n\tState: RUNNING\n\tName: .*\n\tDescription: ${DESC}" | grep -P '\tVolume: ' | grep -o '[0-9]\+%' | head -n 1
+REGEX="Sink #${sink_id}+\n\tState: (RUNNING|SUSPENDED)\n\tName: .*\n\tDescription: ${DESC}"
 
+# print out the volume after any changes
+pactl list | pcregrep -A 10 -M "${REGEX}" | grep -P '\tVolume: ' | grep -o '[0-9]\+%' | head -n 1

a940780e957c4e5be073444f13a72b127472e7a4
Nathan Overall <git@shweppsie.com>
2012-07-21 23:51:39 +1200

    [awesome] Deal with changes to pulseaudio

--- awesome/fry/vol.sh
+++ awesome/fry/vol.sh
@@ -3,7 +3,7 @@
 # Description of device
 DESC="Built-in Audio Analog Stereo"
 
-REGEX="Sink #[0-9]+\n\tState: (RUNNING|SUSPENDED)\n\tName: .*\n\tDescription: ${DESC}"
+REGEX="Sink #[0-9]+\n\tState: (IDLE|RUNNING|SUSPENDED)\n\tName: .*\n\tDescription: ${DESC}"
 
 [[ $2 ]] && increment=$2 || increment=1
 
@@ -17,7 +17,7 @@ elif [[ "$1" == "up" ]]; then
 	pactl set-sink-volume $sink_id -- +${increment}% > /dev/null
 fi
 
-REGEX="Sink #${sink_id}+\n\tState: (RUNNING|SUSPENDED)\n\tName: .*\n\tDescription: ${DESC}"
+REGEX="Sink #${sink_id}+\n\tState: (IDLE|RUNNING|SUSPENDED)\n\tName: .*\n\tDescription: ${DESC}"
 
 # print out the volume after any changes
 pactl list | pcregrep -A 10 -M "${REGEX}" | grep -P '\tVolume: ' | grep -o '[0-9]\+%' | head -n 1

27497dd7d33a546c3137e248046a64e5cf4409ec
Nathan Overall <git@shweppsie.com>
2013-06-12 14:24:37 +1200

    Fix bug in link script

--- vim/link.py
+++ vim/link.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-import os,sys
+import os,sys, shutil
 
 filename = "vimrc"
 dest = "~/.vimrc"
@@ -19,8 +19,10 @@ dest = "~/.vim"
 
 dest = os.path.expanduser(dest)
 
-if os.path.exists(dest) or os.path.islink(dest):
-	os.unlink(dest)
+if os.path.islink(dest):
+    os.unlink(dest)
+elif os.path.exists(dest):
+    shutil.rmtree(dest)
 
 workingdir = os.path.abspath(os.path.dirname(sys.argv[0]))
 

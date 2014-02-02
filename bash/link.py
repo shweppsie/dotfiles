c9cef9e9d078ea910310235c904783deb800ebd3
Nathan Overall <git@shweppsie.com>
2014-02-02 13:55:26 +1300

    Add version control support to PS1

--- bash/link.py
+++ bash/link.py
@@ -14,3 +14,15 @@ workingdir = os.path.abspath(os.path.dirname(sys.argv[0]))
 
 os.symlink(os.path.join(workingdir,filename),dest)
 
+filename = "vcprompt"
+dest = "~/.bin/vcprompt"
+
+dest = os.path.expanduser(dest)
+
+if os.path.exists(dest) or os.path.islink(dest):
+	os.unlink(dest)
+
+workingdir = os.path.abspath(os.path.dirname(sys.argv[0]))
+
+os.symlink(os.path.join(workingdir,filename),dest)
+

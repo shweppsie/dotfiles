d930fead775c6505e1a7277db88ddb3fcd5a7eba
Nathan Overall <nathan.overall@lightwire.co.nz>
2018-11-15 15:00:14 +1300

    Fix errors in symlink creating and fix tabbing.

--- vim/link.py
+++ vim/link.py
@@ -3,21 +3,21 @@
 import os,sys, shutil
 
 for src in ["vimrc", "vim"]:
-	dst = "~/.%s" % src
+    dst = "~/.%s" % src
 
-	# get current dir
-	workingdir = os.path.abspath(os.path.dirname(sys.argv[0]))
+    # get current dir
+    workingdir = os.path.abspath(os.path.dirname(sys.argv[0]))
 
-	# expand paths
-	dst = os.path.expanduser(dst)
-	src = os.path.join(workingdir,src)
+    # expand paths
+    dst = os.path.expanduser(dst)
+    src = os.path.join(workingdir,src)
 
-	# get relative path
-	src = os.path.relpath(src,dst)
+    # get relative path
+    src = os.path.relpath(src,os.path.dirname(dst))
 
-	# unlink old files
-	if os.path.exists(dst) or os.path.islink(dst):
-		os.unlink(dst)
+    # unlink old files
+    if os.path.exists(dst) or os.path.islink(dst):
+        os.unlink(dst)
 
-	print('Symlinking "%s" to "%s"' % (dst,src))
-	os.symlink(src,dst)
+    print('Symlinking "%s" to "%s"' % (dst,src))
+    os.symlink(src,dst)
